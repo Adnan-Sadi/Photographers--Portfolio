5a0291c654371170b1e6f90cafb6ea2988b703d2
nusrat_khan <86890637+nusratk12@users.noreply.github.com>
2021-12-26 15:24:16 +0600

    updated route  for profileindex

--- Photographers'-Portfolio/routes/web.php
+++ Photographers'-Portfolio/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Intervention\Image\Facades\Image;
 use App\Http\Controllers\NewsfeedController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\GalleryController;
-
+use App\Http\Controllers\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,11 +25,19 @@ Route::post('/registration',[LoginController::class, 'Register']);
 Route::get('/login',[LoginController::class, 'Login'])->name('auth.login');
 Route::post('/login',[LoginController::class, 'ValidateLogin'])->name('auth.ValidateLogin');
 
+// Profile routes
+Route::get ('/user/{user}', [Profile::class, 'index'])->name('user.index') ;
+Route::get('/ user/{user}/edit', 'ProfilesController@edit')->name('user.edit');
+Route::patch('/ user/{user}/update', 'ProfilesController@update')->name('user.update');
+
+
+
 Route::get('/photo/{photo}', [PhotoController::class, 'index'])->name('photo.index');
 Route::post('/photo-upload', [PhotoController::class, 'photoUpload'])->name('photo.photo_upload');
 Route::get('/logout',[LoginController::class, 'Logout']);
 
 Route::get('/gallery',[GalleryController::class, 'gallery']);
+
 
 
 Route::group(['middleware'=>['session']], function(){
