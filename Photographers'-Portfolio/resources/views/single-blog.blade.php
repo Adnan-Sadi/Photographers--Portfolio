e92fdf78d72342fbe45bcdf83be7715d5fbe42c0
Afsana Chowdhury Reevu <reevuafsana@gmail.com>
2021-12-28 21:22:15 +0600

    slightly modified the blog viewing page layout

--- Photographers'-Portfolio/resources/views/single-blog.blade.php
+++ Photographers'-Portfolio/resources/views/single-blog.blade.php
@@ -74,32 +74,7 @@
     </div>
     <!-- Blog Details Area End -->
 
-    <!-- Pager Area Start -->
-    <div class="alime-pager-area section-padding-0-80">
-        <div class="container">
-            <div class="row no-gutters">
-                <!-- Previous -->
-                <div class="col-6">
-                    <div class="previous bg-img" style="background-image: url(img/bg-img/5.jpg);">
-                        <div class="overlay-content d-flex align-items-center justify-content-center">
-                            <a href="#" class="post-title">Previous Post</a>
-                            <a href="#" class="previous-link"><i class="arrow_left"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Next -->
-                <div class="col-6">
-                    <div class="next bg-img" style="background-image: url(img/bg-img/49.jpg);">
-                        <div class="overlay-content d-flex align-items-center justify-content-center">
-                            <a href="#" class="post-title">Next Post</a>
-                            <a href="#" class="next-link"><i class="arrow_right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Pager Area End -->
+
 
     <!-- Comment Area Start -->
     <div class="comment-area section-padding-80">
