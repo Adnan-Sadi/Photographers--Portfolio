bbe8415d6616593832047bcaef0a55c50615d8b3
Adnan-Sadi <57232041+Adnan-Sadi@users.noreply.github.com>
2022-01-02 16:58:20 +0600

    Update on Photo Upload Unit Test
    Added two  missing test methods.

--- Photographers'-Portfolio/tests/Unit/PhotoUploadTest.php
+++ Photographers'-Portfolio/tests/Unit/PhotoUploadTest.php
@@ -189,5 +189,59 @@ class PhotoUploadTest extends TestCase
         $this->assertEquals($expected,$validates->fails());
     }
 
+    /**
+     * Tests what happens when the photo size does not exceed limit.
+     * The expected result is that the validation result does not fail.
+     */
+    public function testPhotoSizeDoesNotExceedLimit()
+    {
+        //the expected value is validation fails
+        $expected = false;
+
+        $rules = array(
+            'caption' => ['string','nullable'],
+            'photo'   => ['required','mimes:png,jpg,jpeg','max:9048'],
+        );
+
+        Storage::fake('avatars');
+        
+        $request = new Request([
+            'caption' => null,
+            'photo' => UploadedFile::fake()->image('avatar.jpeg')->size(9048)
+        ]);
+        
+        //checking validation rules against all variables in $request
+        $validates = Validator::make($request->all(), $rules);
+
+        $this->assertEquals($expected,$validates->fails());
+    }
+
+    /**
+     * Tests what happens when the photo size is too large.
+     * The expected result is that the validation result will fail.
+     */
+    public function testPhotoSizeIsTooLarge()
+    {
+        //the expected value is validation fails
+        $expected = True;
+
+        $rules = array(
+            'caption' => ['string','nullable'],
+            'photo'   => ['required','mimes:png,jpg,jpeg','max:9048'],
+        );
+
+        Storage::fake('avatars');
+        
+        $request = new Request([
+            'caption' => null,
+            'photo' => UploadedFile::fake()->image('avatar.jpg')->size(9049)
+        ]);
+        
+        //checking validation rules against all variables in $request
+        $validates = Validator::make($request->all(), $rules);
+
+        $this->assertEquals($expected,$validates->fails());
+    }
+
     
 }
