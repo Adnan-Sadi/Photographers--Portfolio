cbcfb0ba21b3ad0a75db18dbe8b166a8837aa527
nusrat_khan <86890637+nusratk12@users.noreply.github.com>
2022-01-02 19:01:47 +0600

    Update Profile.php

--- Photographers'-Portfolio/app/Http/Controllers/Profile.php
+++ Photographers'-Portfolio/app/Http/Controllers/Profile.php
@@ -5,50 +5,75 @@
 namespace App\Http\Controllers;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Validator;
-use App\Models\User;
+
 
 
 use Illuminate\Support\Facades\Cache;
 use Session;
-
+use App\Models\User;
 use Illuminate\Http\Request;
-
 class Profile extends Controller
 {
-    protected $user;
-    public function index(User $user) {
-
-
+/**
+ *  profile(): Display the view of an individual profile.
+ * When the user found logged in shows the user data
+ *
+ * @param User $user
+ * @return void
+ */
+    public function  profile( User $user ) {
 
         $loggedInUser = Session::get('u_id');
         $userName = Session::get('username');
+        $fullname=Session::get('full_name');
+        $email=Session::get(' email');
 
-        $follows = ($loggedInUser) ? $loggedInUser->following->contains($user->u_id) : false;
-
-        $showFollowBtn = true;
         if ($loggedInUser->u_id == $user->u_id)
-        $showFollowBtn = false;
 
-        return view('user' ,compact('user', 'showFollowBtn'));
-    }
-    public function edit(User $user)
-    {
-        $this->authorize('update', $user->profile);
-        return view('editprofile', compact('user'));
-    }
-
-    public function update(User $user)
-    {
-        $this->authorize('update', $user->profile);
-
-        $data = request()->validate([
-            'title' => '',
-            'bio' => '',
-            'url' => '',
-            'image' => ''
-        ]);
+        return view('profile')->with('username',$userName)
+        ->with('full_name',$fullname)->with('email',$email) ;
 
 
-        return redirect('/user/' . $user->u_id);
-    }
+
+        /**
+         * Validate user for update input data
+         */
+
+        public function update(User $user)
+        {
+            $this->authorize('update',  $user->u_id);
+
+            $data = request()->validate([
+
+                'bio' => '',
+
+                'profile_image' => ''
+            ]);
+    /**
+     * user image update
+     * Storing the image path through save()
+     */
+
+            if (request('profile_image')) {
+                $imagePath = request('image')->store('user', 'public');
+
+                $image = Image::make(public_path("storage/{$imagePath}"))->fit('512', '512');
+                $image->save();
+
+                $imageArr = ['profile_image' => $imagePath];
+            }
+
+            if ($loggedInUser->u_id == $user->u_id)
+                update(array_merge(
+                $data, $imageArr ?? [])
+            );
+
+            return redirect('/profile/' . $user->u_id);
+        }
+
+
+
 }
+
+
+ 
